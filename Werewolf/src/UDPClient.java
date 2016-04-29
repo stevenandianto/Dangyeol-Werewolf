b82e21e7eac67210f3aede93074d3b494b5cf4bd
Steven Andianto <stevenandianto@gmail.com>
2016-04-30 00:05:25 +0700

    Inisiasi

---
+++ Werewolf/src/UDPClient.java
@@ -0,0 +1,29 @@
+/**
+ * Created by user on 29/04/2016.
+ */
+import java.io.*;
+import java.net.*;
+
+class UDPClient
+{
+    public static void main(String args[]) throws Exception
+    {
+    while(true) {
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket(9875);
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("FROM SERVER:" + modifiedSentence);
+        clientSocket.close();
+    }
+    }
+}
